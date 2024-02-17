13a8e6bc598530fe7d37ee79e546ed723129b148
Kerem Karaman <keremkaraman91@gmail.com>
2024-02-17 13:15:40 +0300

    Initial Commit

---
+++ ToDoApp-SwiftUI-FireBase/ToDoApp_SwiftUI_FireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_SwiftUI_FireBaseApp.swift
+//  ToDoApp-SwiftUI-FireBase
+//
+//  Created by Kerem Karaman on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp_SwiftUI_FireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
